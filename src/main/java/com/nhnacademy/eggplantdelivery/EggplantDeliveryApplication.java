246e5f9d6ed560147519b8f67c8324caed7417e5
corock <hoongineer@gmail.com>
2022-07-26 16:07:23 +0900

    Create a Spring Boot project with Maven

---
+++ src/main/java/com/nhnacademy/eggplantdelivery/EggplantDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.eggplantdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggplantDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EggplantDeliveryApplication.class, args);
+    }
+
+}

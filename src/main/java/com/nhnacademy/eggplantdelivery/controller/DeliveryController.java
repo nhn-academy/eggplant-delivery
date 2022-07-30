9fb3198217e29d1d4e18a04581669e2731444946
bunsung92 <bunsung92@naver.com>
2022-07-30 18:36:34 +0900

    fix: 코멘트 내용 수정 (#9)

--- src/main/java/com/nhnacademy/eggplantdelivery/controller/DeliveryController.java
+++ src/main/java/com/nhnacademy/eggplantdelivery/controller/DeliveryController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 배송 요청관련 처리를 위한 컨트롤러 입니다.
+ *
+ * @version 1.0.0
  */
 @RestController
 @RequestMapping("/eggplant-delivery")

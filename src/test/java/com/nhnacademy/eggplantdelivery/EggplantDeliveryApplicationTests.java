0cfff40f89e898ff54bc3658552fcea027b0d991
bunsung92 <bunsung92@naver.com>
2022-08-03 11:51:40 +0900

    test: Service 운송장 번호 추가 테스트(#22)

--- src/test/java/com/nhnacademy/eggplantdelivery/EggplantDeliveryApplicationTests.java
+++ src/test/java/com/nhnacademy/eggplantdelivery/EggplantDeliveryApplicationTests.java
@@ -1,13 +1,17 @@
-//package com.nhnacademy.eggplantdelivery;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class EggplantDeliveryApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.nhnacademy.eggplantdelivery;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class EggplantDeliveryApplicationTests {
+
+    @Test
+    void contextLoads(ApplicationContext context) {
+        assertThat(context).isNotNull();
+    }
+
+}

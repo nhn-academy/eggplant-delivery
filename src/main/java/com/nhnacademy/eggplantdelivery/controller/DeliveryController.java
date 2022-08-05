ed1dd7ca55759d71cd3f326488d5643e5869a85e
bunsung92 <bunsung92@naver.com>
2022-08-05 22:04:39 +0900

    test: 배송상태 조회, 배송상태 수정 Service 테스트 코드 추가 (#31)
    Co-authored-by: @JoisFe <rhsnqk@gmail.com>

--- src/main/java/com/nhnacademy/eggplantdelivery/controller/DeliveryController.java
+++ src/main/java/com/nhnacademy/eggplantdelivery/controller/DeliveryController.java
@@ -31,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeliveryController {
 
     private final Sender sender;
+
     private final DeliveryService service;
 
     /**
--- src/test/java/com/nhnacademy/eggplantdelivery/service/impl/DefaultDeliveryServiceTest.java
+++ src/test/java/com/nhnacademy/eggplantdelivery/service/impl/DefaultDeliveryServiceTest.java
@@ -1,12 +1,16 @@
 package com.nhnacademy.eggplantdelivery.service.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.nhnacademy.eggplantdelivery.adaptor.DeliveryAdaptor;
+import com.nhnacademy.eggplantdelivery.dto.request.DeliveryStatusUpdateRequestDto;
 import com.nhnacademy.eggplantdelivery.dto.request.OrderInfoRequestDto;
+import com.nhnacademy.eggplantdelivery.dto.response.DeliveryInfoStatusResponseDto;
 import com.nhnacademy.eggplantdelivery.entity.DeliveryInfo;
 import com.nhnacademy.eggplantdelivery.entity.status.Status;
 import com.nhnacademy.eggplantdelivery.module.Sender;
@@ -14,6 +18,9 @@ import com.nhnacademy.eggplantdelivery.repository.DeliveryInfoRepository;
 import com.nhnacademy.eggplantdelivery.service.DeliveryService;
 import com.nhnacademy.eggplantdelivery.utill.UuidVer5Generator;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,6 +29,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(SpringExtension.class)
 @Import(DefaultDeliveryService.class)
@@ -75,12 +83,56 @@ class DefaultDeliveryServiceTest {
     }
 
     @Test
-    void sendTrackingNo() {
+    void testSendTrackingNo() {
         doNothing().when(deliveryAdaptor).sendTrackingNo(orderInfoRequestDto);
 
         service.sendTrackingNo(orderInfoRequestDto);
 
         verify(deliveryAdaptor).sendTrackingNo(orderInfoRequestDto);
+    }
+
+    @Test
+    void testSendUpdateStatus() {
+        UUID uuid = UuidVer5Generator.ver5UuidFromNamespaceAndBytes(("Host" + "OrderNo").getBytes(
+            StandardCharsets.UTF_8));
+
+        DeliveryInfo deliveryInfo = DeliveryInfo.builder()
+                                                .trackingNo(uuid.toString())
+                                                .status(Status.DELIVERING)
+                                                .receiverName(orderInfoRequestDto.getReceiverName())
+                                                .receiverAddress(orderInfoRequestDto.getReceiverAddress())
+                                                .receiverPhone(orderInfoRequestDto.getReceiverPhone())
+                                                .orderNo(orderInfoRequestDto.getOrderNo())
+                                                .shopHost(orderInfoRequestDto.getShopHost())
+                                                .build();
+
+        DeliveryStatusUpdateRequestDto deliveryStatusUpdateRequestDto = new DeliveryStatusUpdateRequestDto();
+
+        ReflectionTestUtils.setField(deliveryStatusUpdateRequestDto, "trackingNo", uuid.toString());
+        ReflectionTestUtils.setField(deliveryStatusUpdateRequestDto, "status", Status.DELIVERING);
+
+        when(deliveryInfoRepository.findById(anyString())).thenReturn(Optional.ofNullable(deliveryInfo));
+
+        Objects.requireNonNull(deliveryInfo).updateStatus(deliveryStatusUpdateRequestDto.getStatus());
+
+        doNothing().when(deliveryAdaptor).sendUpdateStatus(any());
+
+        service.sendUpdateStatus(deliveryStatusUpdateRequestDto);
+
+        verify(deliveryAdaptor).sendUpdateStatus(any());
+
+    }
+
+    @Test
+    void testRetrieveDeliveryStatus() {
+        List<DeliveryInfoStatusResponseDto> deliveryInfoStatusResponseDtos =
+            List.of(new DeliveryInfoStatusResponseDto());
+        when(deliveryInfoRepository.retrieveDeliveryStatus()).thenReturn(deliveryInfoStatusResponseDtos);
+
+        List<DeliveryInfoStatusResponseDto> deliveryInfoStatusResponseDtoList = service.retrieveDeliveryStatus();
+
+        assertThat(deliveryInfoStatusResponseDtoList).isEqualTo(deliveryInfoStatusResponseDtos);
+        verify(deliveryInfoRepository).retrieveDeliveryStatus();
     }
 
 }

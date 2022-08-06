cc0b49a52549a1a811e3abf95e185084856db0e0
bunsung92 <bunsung92@naver.com>
2022-08-06 15:19:01 +0900

    test: 배송 상태 조회 (#35)
    Co-authored-by: @JoisFe <rhsnqk@gmail.com>

--- src/test/java/com/nhnacademy/eggplantdelivery/controller/DeliveryControllerTest.java
+++ src/test/java/com/nhnacademy/eggplantdelivery/controller/DeliveryControllerTest.java
@@ -7,11 +7,14 @@ import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.eggplantdelivery.dto.request.DeliveryStatusUpdateRequestDto;
 import com.nhnacademy.eggplantdelivery.dto.request.OrderInfoRequestDto;
+import com.nhnacademy.eggplantdelivery.dto.response.DeliveryInfoStatusResponseDto;
 import com.nhnacademy.eggplantdelivery.module.Sender;
 import com.nhnacademy.eggplantdelivery.repository.DeliveryInfoRepository;
 import com.nhnacademy.eggplantdelivery.service.DeliveryService;
@@ -87,7 +90,18 @@ class DeliveryControllerTest {
 
     @Test
     @DisplayName("배송 상태 조회")
-    void testRetrieveDeliveryStatus() {
+    void testRetrieveDeliveryStatus() throws Exception {
+        DeliveryInfoStatusResponseDto deliveryInfoStatusResponseDto = new DeliveryInfoStatusResponseDto();
+        List<DeliveryInfoStatusResponseDto> deliveryInfoStatusResponseDtoList = List.of(deliveryInfoStatusResponseDto);
+
+        when(service.retrieveDeliveryStatus()).thenReturn(deliveryInfoStatusResponseDtoList);
+
+        String jsonRequest = mapper.writeValueAsString(deliveryInfoStatusResponseDtoList);
+
+        mockMvc.perform(get("/eggplant-delivery/delivery-info-status")
+            .contentType(APPLICATION_JSON)
+            .content(jsonRequest))
+            .andExpect(status().isOk());
 
     }
 

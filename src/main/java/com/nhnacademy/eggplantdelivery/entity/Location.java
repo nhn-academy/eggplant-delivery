39659e57535fcaac79fcb5d5a39bce59252b024e
bunsung92 <bunsung92@naver.com>
2022-08-15 00:55:47 +0900

    refactor: location entity 수정 (#55)

--- src/main/java/com/nhnacademy/eggplantdelivery/entity/Location.java
+++ src/main/java/com/nhnacademy/eggplantdelivery/entity/Location.java
@@ -1,15 +1,20 @@
 package com.nhnacademy.eggplantdelivery.entity;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
@@ -23,17 +28,36 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Location {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "location_no")
-    private Long locationNo;
+    @EmbeddedId
+    private Pk pk;
 
+    @MapsId(value = "trackingNo")
     @ManyToOne
     @JoinColumn(name = "tracking_no")
     private DeliveryInfo deliveryInfo;
 
     @Column(name = "arrival_time")
-    @NotNull(message = "도착시간은 필수 입니다.")
     private LocalDateTime arrivalTime;
+
+    /**
+     * Location 고유키, 복합키를 담은 클래스 입니다.
+     *
+     * @author 김훈민, 조재철
+     */
+    @Embeddable
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @EqualsAndHashCode
+    public static class Pk implements Serializable {
+
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "location_no")
+        private Long locationNo;
+
+        @Column(name = "tracking_no")
+        private String trackingNo;
+
+    }
 
 }

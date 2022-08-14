960283237df3748dd361a8d115da8f1720378ea3
bunsung92 <bunsung92@naver.com>
2022-08-14 16:50:49 +0900

    refactor: Location 엔티티 연관관계 설정 수정 (#55)

--- src/main/java/com/nhnacademy/eggplantdelivery/entity/Location.java
+++ src/main/java/com/nhnacademy/eggplantdelivery/entity/Location.java
@@ -1,21 +1,15 @@
 package com.nhnacademy.eggplantdelivery.entity;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
@@ -29,10 +23,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Location {
 
-    @EmbeddedId
-    private Pk pk;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_no")
+    private Long locationNo;
 
-    @MapsId(value = "trackingNo")
     @ManyToOne
     @JoinColumn(name = "tracking_no")
     private DeliveryInfo deliveryInfo;
@@ -40,28 +35,5 @@ public class Location {
     @Column(name = "arrival_time")
     @NotNull(message = "도착시간은 필수 입니다.")
     private LocalDateTime arrivalTime;
-
-    /**
-     * Location 고유키, 복합키를 담은 클래스 입니다.
-     *
-     * @author 김훈민, 조재철
-     */
-    @Embeddable
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Getter
-    @EqualsAndHashCode
-    public static class Pk implements Serializable {
-
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "location_no")
-        @NotNull(message = "위치 번호는 필수 입니다.")
-        private Long locationNo;
-
-        @Column(name = "tracking_no")
-        @NotNull(message = "운송장 번호는 필수 입니다.")
-        private String trackingNo;
-
-    }
 
 }

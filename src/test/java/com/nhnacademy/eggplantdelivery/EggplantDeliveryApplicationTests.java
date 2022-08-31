151bf6e45c929dce1d9695e492cc23563c81cdd9
bunsung92 <bunsung92@naver.com>
2022-08-31 10:22:18 +0900

    fix: 테스트 코드 프로파일 추가 (#67)

--- src/test/java/com/nhnacademy/eggplantdelivery/EggplantDeliveryApplicationTests.java
+++ src/test/java/com/nhnacademy/eggplantdelivery/EggplantDeliveryApplicationTests.java
@@ -5,7 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("dev")
 @SpringBootTest
 class EggplantDeliveryApplicationTests {
 
